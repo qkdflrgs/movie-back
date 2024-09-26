4bc3cae0f3c9e05bcfbc575072df4dda375e8439
qkdflrgs <qkdflrgs@gmail.com>
2024-09-27 00:52:40 +0900

    feat(transformer): add Movie entity with class-validator

--- src/movie/entities/movie.entity.ts
+++ src/movie/entities/movie.entity.ts
@@ -1,1 +1,15 @@
-export class Movie {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class Movie {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  genre: string;
+}
