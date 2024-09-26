1f726ebadaea8ef9e6317651b09cbfe25a7968d4
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-09-27 00:18:22 +0900

    [Feat] (validator): add Class Validator to movie dto (#2)
    * feat(validator): install class-transformer and class-validator
    
    * feat(validator): add ValidationPipe to App
    
    * feat(validator): add class validator to CreateMovieDto
    
    * feat(validator): add class validator to UpdateMovieDto

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,7 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from './create-movie.dto';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+  @IsNotEmpty()
+  @IsOptional()
   title?: string;
+
+  @IsNotEmpty()
+  @IsOptional()
   genre?: string;
 }
