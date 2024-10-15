ffdcfa6db4d61fdb2619cc072665c79fd1c14e41
qkdflrgs <qkdflrgs@gmail.com>
2024-10-15 16:25:05 +0900

    feat(relation): add Director, Genre Entities to MovieModule

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -4,9 +4,11 @@ import { MovieController } from './movie.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movie } from './entities/movie.entity';
 import { MovieDetail } from './entities/movie-detail.entity';
+import { Director } from 'src/director/entities/director.entity';
+import { Genre } from 'src/genre/entities/genre.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Movie, MovieDetail])],
+  imports: [TypeOrmModule.forFeature([Movie, MovieDetail, Director, Genre])],
   controllers: [MovieController],
   providers: [MovieService],
 })
