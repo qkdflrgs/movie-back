56044d41c70894012b1ff6253ce309e0561fbd54
qkdflrgs <qkdflrgs@gmail.com>
2024-10-15 16:26:33 +0900

    feat(relation): refactoring Movie entity with Relationships

--- src/movie/entities/movie.entity.ts
+++ src/movie/entities/movie.entity.ts
@@ -2,26 +2,41 @@ import {
   Column,
   Entity,
   JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { Metadata } from './metadata.entity';
+import { Metadata } from '../../common/entities/metadata.entity';
 import { MovieDetail } from './movie-detail.entity';
+import { Director } from 'src/director/entities/director.entity';
+import { Genre } from 'src/genre/entities/genre.entity';
 
 @Entity()
 export class Movie extends Metadata {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
   title: string;
 
-  @Column()
-  genre: string;
+  @ManyToMany(() => Genre, (genre) => genre.movies)
+  @JoinTable()
+  genres: Genre[];
 
   @OneToOne(() => MovieDetail, (movieDetail) => movieDetail.id, {
     cascade: true,
+    nullable: false,
   })
   @JoinColumn()
   detail: MovieDetail;
+
+  @ManyToOne(() => Director, (director) => director.id, {
+    cascade: true,
+    nullable: false,
+  })
+  director: Director;
 }
